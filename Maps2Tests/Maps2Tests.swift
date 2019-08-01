f33947d3baf7a50acbf55093097ee1fb48a2fa5f
mohammed eloksh <mmaeloksh@gmail.com>
2019-08-01 14:07:06 +0200

    ELOKSH PROJECT

---
+++ Maps2Tests/Maps2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Maps2Tests.swift
+//  Maps2Tests
+//
+//  Created by macbookpro on 8/1/19.
+//  Copyright © 2019 macbookpro. All rights reserved.
+//
+
+import XCTest
+@testable import Maps2
+
+class Maps2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
